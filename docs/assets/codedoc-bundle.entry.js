4270a135cd8e2a99bb5293b0aab7431f2a550fdf
nakednous <nakednous@gmail.com>
2020-11-20 21:35:11 +0000

    Deploying to gh-pages from  @ 437b63304aa1fea94824836560ef0559249b94d0 🚀

--- docs/assets/codedoc-bundle.entry.js
+++ docs/assets/codedoc-bundle.entry.js
@@ -14,12 +14,14 @@ import { tocHighlight } from '/home/runner/work/p5.quadrille.js/p5.quadrille.js/
 import { postNavSearch } from '/home/runner/work/p5.quadrille.js/p5.quadrille.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
 import { copyLineLinks } from '/home/runner/work/p5.quadrille.js/p5.quadrille.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
 import { gatherFootnotes } from '/home/runner/work/p5.quadrille.js/p5.quadrille.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
+import { TabSelector } from '/home/runner/work/p5.quadrille.js/p5.quadrille.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/tabs/selector.js';
 import { GithubSearch } from '/home/runner/work/p5.quadrille.js/p5.quadrille.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/misc/github/search.js';
 import { ToCToggle } from '/home/runner/work/p5.quadrille.js/p5.quadrille.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
 import { DarkModeSwitch } from '/home/runner/work/p5.quadrille.js/p5.quadrille.js/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
 import { ConfigTransport } from '/home/runner/work/p5.quadrille.js/p5.quadrille.js/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
 
 const components = {
+  'LiW3+ihxAP1kGrRwzDsT6g==': TabSelector,
   'ROYBRx6H9Nd50i6uNpoCOA==': GithubSearch,
   'd6M+BB17k7SZ9Q4Fl3imZQ==': ToCToggle,
   'RyJFI7P2FKthOgKgXdYxkw==': DarkModeSwitch,
